9719b7c5cd873161449eb90c33ccc13f79a25400
lsri13 <lalitha.s.srikumar@gmail.com>
2025-12-03 16:24:04 +0100

    für das spiel

---
+++ board.c
@@ -0,0 +1,4 @@
+#include "header.h"
+
+void board_init(Board *b);
+void board_print(const Board *b);
---
+++ game.c
@@ -0,0 +1,5 @@
+#include "header.h"
+
+int board_drop_piece(Board *b, int col, char piece); // col: 0-basiert, Rückgabe: Zeile oder -1
+int board_is_full(const Board *b);
+int board_check_win(const Board *b, char piece); // 1 = gewonnen, 0 = noch nicht

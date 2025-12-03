cab50c22312616e271f8910f8794bf18eafbe855
lsri13 <lalitha.s.srikumar@gmail.com>
2025-12-03 15:32:39 +0100

    testneu

---
+++ testneu.c
@@ -0,0 +1,13 @@
+/*
+Datei herunterladen: im Terminal
+git pull
+
+
+Datei hochladen: im Terminal
+git status (hier sind alle Dateien zu sehen, die untracked sind)
+git add dateiname.c
+git commit
+git push
+*/
+
+int main();

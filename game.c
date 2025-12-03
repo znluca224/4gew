64c3f6b18510a73efb7d15926fff3b4767128636
znluca224 <luca.zuin@stud.hs-hannover.de>
2025-12-03 16:28:40 +0100

    änderungen in game

--- game.c
+++ game.c
@@ -1,5 +1,5 @@
 #include "header.h"
 
-int board_drop_piece(Board *b, int col, char piece); // col: 0-basiert, Rückgabe: Zeile oder -1
+int drop_piece(Board *b, int col, char piece); // col: 0-basiert, Rückgabe: Zeile oder -1
 int board_is_full(const Board *b);
 int board_check_win(const Board *b, char piece); // 1 = gewonnen, 0 = noch nicht

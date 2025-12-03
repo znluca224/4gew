fd4bc458d86b0de87bdc9516023c2ff2d1516257
dk84dk <z.game859@passmail.net>
2025-12-03 17:12:01 +0100

    modified:   header.h corrected variable names

--- header.h
+++ header.h
@@ -4,14 +4,15 @@
 #define ROWS 6
 #define COLS 7
 
-typedef struct {
-    char cells[ROWS][COLS];  // ' 'leeres Feld, 'X' Spieler 1 oder 'O' Spieler 2
+typedef struct
+{
+    char cells[ROWS][COLS]; // ' 'leeres Feld, 'X' Spieler 1 oder 'O' Spieler 2
 } Board;
 
-void board_init(Board *b);
-void board_print(const Board *b);
-int  board_drop_piece(Board *b, int col, char piece); // col: 0-basiert, Rückgabe: Zeile oder -1
-int  board_is_full(const Board *b);
-int  board_check_win(const Board *b, char piece);     // 1 = gewonnen, 0 = noch nicht
+void clearBoard(int[][] GBoard);
+void printBoard(int[][] GBoard, char piece1, char piece2);
+int board_drop_piece(Board *b, int col, char piece); // col: 0-basiert, Rückgabe: Zeile oder -1
+int board_is_full(const Board *b);
+int board_check_win(const Board *b, char piece); // 1 = gewonnen, 0 = noch nicht
 
 #endif
